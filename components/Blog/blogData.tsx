9a16a64e5b3a859dcd5ca8465a91e77e25d6530e
Sarth Rajain <96329474+InvisibleTroll54219@users.noreply.github.com>
2023-07-05 18:07:08 +0530

    member details pushed

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -1,46 +1,214 @@
 import { Blog } from "@/types/blog";
 
 const blogData: Blog[] = [
+  // Club Secratary 
   {
     id: 1,
-    title: "Best UI components for modern websites",
+    title: "Vansh Yadav",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+      "Club Secretary, Aerodynamics Team Head",
+    image: "./images/blog/IMG_20220307_183329__01__01 - Yadav Vansh Suresh.jpg",
 
   },
+  // Aerodynamics Team
   {
     id: 2,
-    title: "Best UI components for modern websites",
+    title: "Rudra Rudra",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+      "Team Member",
+    image: "./images/blog/Rudra - Rudra Rudra.jpg",
 
   },
   {
     id: 3,
-    title: "Best UI components for modern websites",
+    title: "Ankit Kumar",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+      "Team Member",
+    image: "./images/blog/IMG_20230112_141607 - Ankit Kumar.jpg",
 
   },
+  // Powertrain Team
   {
     id: 4,
-    title: "Best UI components for modern websites",
+    title: "Harsh Vardhan Chowdhary",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+      "Team Head",
+    image: "./images/blog/IMG_20230520_124748 - Harsh Vardhan Chowdhary.jpg",
 
   },
   {
     id: 5,
-    title: "Best UI components for modern websites",
+    title: "Devansh Jandrasupalli",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg",
+      "Team Member",
+    image: "./images/blog/WhatsApp Image 2023-06-25 at 21.57.35 Devansh.jpg",
 
   },
-  
+  {
+    id: 6,
+    title: "Tejas Mhaiskar",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/ingene photo - Tejas Ajit Mhaiskar.jpg",
+
+  },
+  // Suspension Team
+  {
+    id: 7,
+    title: "Arindam Thander",
+    paragraph:
+      "Team Head",
+    image: "./images/blog/IMG_20230321_192316 - Arindam Thander.jpg",
+
+  },
+  {
+    id: 8,
+    title: "Varun Limaye",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/IMG_20221126_220957~2 - Varun Shantanu Limaye.jpg",
+
+  },
+  {
+    id: 9,
+    title: "Ratish Sinha",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/WhatsApp Image 2023-05-17 at 01.33.35 - Ratish Sinha.jpg",
+
+  },
+  // Brakes Team
+  {
+    id: 10,
+    title: "Varun Limaye",
+    paragraph:
+      "Team Head",
+    image: "./images/blog/IMG_20221126_220957~2 - Varun Shantanu Limaye.jpg",
+
+  },
+  {
+    id: 11,
+    title: "Kute Yash Satish",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/IMG_20230430_141145 - Kute Yash Satish.jpg",
+
+  },
+  {
+    id: 12,
+    title: "Lokesh B Jogi",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/IMG_20230513_171551 - Lokesh B Jogi.jpg",
+
+  },
+  //Chassis Team
+  {
+    id: 13,
+    title: "Hari Prasad P",
+    paragraph:
+      "Team Head",
+    image: "./images/blog/Hari_Prasad_P.jpg",
+
+  },
+  {
+    id: 14,
+    title: "Arindam Thander",
+    paragraph:
+      "Team Member",
+      image: "./images/blog/IMG_20230321_192316 - Arindam Thander.jpg",
+
+  },
+  {
+    id: 15,
+    title: "Varun Limaye",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/IMG_20221126_220957~2 - Varun Shantanu Limaye.jpg",
+
+  },
+  // Steering Team
+  {
+    id: 16,
+    title: "Anurag Goel",
+    paragraph:
+      "Team Head",
+    image: "./images/blog/Screenshot_20230516-093249_Gallery - Anurag Goel.jpg",
+
+  },
+  {
+    id: 17,
+    title: "Harsh Vardhan Chowdhary",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/IMG_20230520_124748 - Harsh Vardhan Chowdhary.jpg",
+
+  },
+  {
+    id: 18,
+    title: "Surzith Raj Pandi",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/IMG_3720 - S. Surzith Raj Pandi.jpg",
+
+  },
+  //Electronis Team
+  {
+    id: 19,
+    title: "Ayush Singhi",
+    paragraph:
+      "Team Head",
+    image: "./images/blog/singhi.jpg",
+
+  },
+  {
+    id: 20,
+    title: "Upamaka S V B S B Abhinay",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/IMG_20221211_105347 - Upamaka S V B S B Abhinay.jpg",
+
+  },
+  {
+    id: 21,
+    title: "Ayush Mallick",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/SAVE_20230515_062900 - Ayush Mallick.jpg",
+
+  },
+  // Outreach / Media Team
+  {
+    id: 22,
+    title: "Manindra Singh Rathore",
+    paragraph:
+      "Team Head",
+    image: "./images/blog/IMG_20230517_163633 - Manindra Singh Rathore.jpg",
+
+  },
+  {
+    id: 23,
+    title: "Gudapareddy Naganandini",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/02584B0C-C50E-4205-A4FD-F6D13D5D1215 - Gudapareddy Naganandini.jpeg",
+
+  },
+  {
+    id: 24,
+    title: "Simoni Tripathi",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/20230516_202329 - Simoni Tripathi.jpg",
+
+  },
+  {
+    id: 25,
+    title: "Bidipta Saha",
+    paragraph:
+      "Team Member",
+    image: "./images/blog/20230516_202329 - Simoni Tripathi.jpg",
+
+  },
 ];
 export default blogData;
